4b8157489c0d74ed346186bc66e52a58993031f7
ivitorh <vitor.silvaa.66@gmail.com>
2023-10-12 22:17:12 -0300

    first commit

--- day-0/day0.js
+++ day-0/day0.js
@@ -1,6 +1,5 @@
 // dada a string: "Olá, tudo bem?" imprima na tela a quantidade de caracteres que essa string possui
 
-
 // minha solução
 var string = 'Olá, tudo bem?';
 console.log(string.length);  // retorna 14
