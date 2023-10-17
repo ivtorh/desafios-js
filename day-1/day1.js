3f9ff237ed70454e37454e4599b69558b631c637
ivitorh <vitor.silvaa.66@gmail.com>
2023-10-17 19:56:15 -0300

    first commit

---
+++ day-1/day1.js
@@ -0,0 +1,6 @@
+// receba o nome do usuário como entrada
+// imprima o nome do usuário com todas as letras em maiúsculo
+
+// minha solução
+var nome = 'vitor hugo';
+console.log(`${nome.toUpperCase()}`);  // retorna 'VITOR HUGO'
