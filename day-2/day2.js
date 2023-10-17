d58556c67438b307e6fc50c614e19f93dff82319
ivitorh <vitor.silvaa.66@gmail.com>
2023-10-17 20:12:57 -0300

    first commit

--- day-2/day2.js
+++ day-2/day2.js
@@ -1,4 +1,4 @@
-// dado o array de números inetiros [2, 3, 1, 5], inverta o array e imprima-os invertidos [5, 1, 3, 2]
+// dado o array de números inteiros [2, 3, 1, 5], inverta o array e imprima-os invertidos [5, 1, 3, 2]
 
 // minha solução
 var array = [2, 3, 1, 5];
