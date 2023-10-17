90497906e4417c31429174d313e382119d2e913e
ivitorh <vitor.silvaa.66@gmail.com>
2023-10-17 20:12:04 -0300

    first commit

---
+++ day-2/day2.js
@@ -0,0 +1,6 @@
+// dado o array de números inetiros [2, 3, 1, 5], inverta o array e imprima-os invertidos [5, 1, 3, 2]
+
+// minha solução
+var array = [2, 3, 1, 5];
+var newArray = array.reverse();
+console.log(newArray);  // retorna [5, 1, 3, 2]  
